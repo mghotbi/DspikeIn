d3573d72c7f8fed85dc12e8d7a5631ae0c9f9ee7
Mitra <mitra.ghotbi@gmail.com>
2024-09-11 11:44:25 -0500

    ExamplePhyloseq

---
+++ ExamplePhyloseq.R
@@ -0,0 +1,94 @@
+library(phyloseq)
+
+# Step 1: Create a taxonomy table 
+taxa_data <- data.frame(
+  OTUID = c("ASV1", "ASV2", "ASV3",    
+            "ASV4", "ASV5", "ASV6",   
+            "ASV7", "ASV8", "ASV9",    
+            "ASV10", "ASV11", "ASV12", 
+            "ASV13", "ASV14", "ASV15", 
+            "ASV16", "ASV17", "ASV18"),
+  Kingdom = rep("Bacteria", 18),
+  Phylum = c("Proteobacteria", "Proteobacteria", "Proteobacteria", 
+             "Proteobacteria", "Proteobacteria", "Proteobacteria", 
+             "Firmicutes", "Firmicutes", "Firmicutes", 
+             "Proteobacteria", "Firmicutes", "Firmicutes", 
+             "Firmicutes", "Firmicutes", "Firmicutes", 
+             "Bacteroidota", "Bacteroidota", "Proteobacteria"),
+  Class = c("Gammaproteobacteria", "Gammaproteobacteria", "Gammaproteobacteria", 
+            "Gammaproteobacteria", "Gammaproteobacteria", "Gammaproteobacteria", 
+            "Clostridia", "Clostridia", "Clostridia", 
+            "Gammaproteobacteria", "Bacilli", "Bacilli", 
+            "Bacilli", "Bacilli", "Clostridia", 
+            "Bacteroidia", "Bacteroidia", "Epsilonproteobacteria"),
+  Order = c("Pseudomonadales", "Pseudomonadales", "Pseudomonadales", 
+            "Enterobacterales", "Enterobacterales", "Enterobacterales", 
+            "Clostridiales", "Clostridiales", "Clostridiales", 
+            "Enterobacterales", "Lactobacillales", "Bacillales", 
+            "Bacillales", "Bacillales", "Clostridiales", 
+            "Bacteroidales", "Bacteroidales", "Campylobacterales"),
+  Family = c("Pseudomonadaceae", "Pseudomonadaceae", "Pseudomonadaceae", 
+             "Enterobacteriaceae", "Enterobacteriaceae", "Enterobacteriaceae", 
+             "Clostridiaceae", "Clostridiaceae", "Clostridiaceae", 
+             "Enterobacteriaceae", "Enterococcaceae", "Staphylococcaceae", 
+             "Listeriaceae", "Bacillaceae", "Clostridiaceae", 
+             "Bacteroidaceae", "Bacteroidaceae", "Helicobacteraceae"),
+  Genus = c("Pseudomonas", "Pseudomonas", "Pseudomonas", 
+            "Escherichia", "Escherichia", "Escherichia", 
+            "Clostridium", "Clostridium", "Clostridium", 
+            "Salmonella", "Enterococcus", "Staphylococcus", 
+            "Listeria", "Bacillus", "Lactobacillus", 
+            "Bacteroides", "Bacteroides", "Helicobacter"),
+  Species = c("Pseudomonas aeruginosa", "Pseudomonas aeruginosa", "Pseudomonas aeruginosa", 
+              "Escherichia coli", "Escherichia coli", "Escherichia coli", 
+              "Clostridium difficile", "Clostridium difficile", "Clostridium difficile", 
+              "Salmonella enterica", "Enterococcus faecalis", "Staphylococcus aureus", 
+              "Listeria monocytogenes", "Bacillus subtilis", "Lactobacillus plantarum", 
+              "Bacteroides fragilis", "Bacteroides vulgatus", "Helicobacter pylori")
+)
+
+# Convert to matrix 
+taxa_matrix <- as.matrix(taxa_data[, -1])
+rownames(taxa_matrix) <- taxa_data$OTUID
+
+# Step 2: Create an OTU table
+otu_data <- round(matrix(
+  c(5.1, 2.3, 1.5,    # Pseudomonas aeruginosa ASV1, ASV2, ASV3
+    12.4, 6.8, 5.9,   # Escherichia coli ASV4, ASV5, ASV6
+    15.2, 7.3, 6.9,   # Clostridium difficile ASV7, ASV8, ASV9
+    12.7, 19, 17.3,   # Salmonella enterica
+    21.4, 10.3, 14.6, # Enterococcus faecalis
+    13.1, 9.8, 4.6,   # Staphylococcus aureus
+    2.5, 1.8, 11.2,   # Listeria monocytogenes
+    7.1, 6.3, 12.7,   # Bacillus subtilis
+    5.8, 5.2, 11.4,   # Lactobacillus plantarum
+    17.1, 15.6, 19.2, # Bacteroides fragilis
+    12.7, 13.8, 12.5, # Bacteroides vulgatus
+    8.3, 7.8, 3.9),   # Helicobacter pylori
+  nrow = 18, ncol = 12, byrow = TRUE,   
+  dimnames = list(taxa_data$OTUID, paste0("Sample", 1:12))
+))
+
+# Step 3: Create sample metadata with 12 samples and 4 rep 
+sample_data <- data.frame(
+  SampleID = paste0("Sample", 1:12),  
+  Category = rep(c("Control", "Extreme Environment", "Normal Condition"), each = 4),  # 4 replicates for each condition
+  row.names = paste0("Sample", 1:12)  
+)
+
+# Step 4: build the phyloseq 
+otu_table_ps <- otu_table(otu_data, taxa_are_rows = TRUE)
+tax_table_ps <- tax_table(taxa_matrix)
+sample_data_ps <- sample_data(sample_data)
+
+physeq <- phyloseq(otu_table_ps, tax_table_ps, sample_data_ps)
+physeq
+
+# Realign the taxonomy table with the OTU table if needed
+physeq@tax_table <- phyloseq::tax_table(phyloseq::tax_table(physeq)[rownames(phyloseq::otu_table(physeq)), ])
+
+# tidy up
+physeq<- tidy_phyloseq(physeq)
+physeq@tax_table
+physeq@sam_data
+physeq@otu_table
