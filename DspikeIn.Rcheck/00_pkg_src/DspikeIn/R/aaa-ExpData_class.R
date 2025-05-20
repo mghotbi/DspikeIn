db3d2f46678ab6db994e2519ad8625801efffb21
mghotbi <mitra.ghotbi@gmail.com>
2025-05-20 22:30:19 +0200

    check3

--- DspikeIn.Rcheck/00_pkg_src/DspikeIn/R/aaa-ExpData_class.R
+++
@@ -1,5 +0,0 @@
-#' @title ExpData Virtual Class
-#' @description This is a virtual superclass to support S4 object structure in DESeqDataSet extension.
-#' @keywords internal
-#' @exportClass ExpData
-setClass("ExpData", contains = "VIRTUAL")
