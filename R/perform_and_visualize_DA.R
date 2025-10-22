e389965605c0b2e5f7ec90d45f85c087ba5b6277
Mitra Ghotbi <mitra.ghotbi@gmail.com>
2025-10-22 20:07:36 +0200

    Wrap DESeq2 example in donttest to prevent Rdb error on build servers

--- R/perform_and_visualize_DA.R
+++ R/perform_and_visualize_DA.R
@@ -52,6 +52,7 @@
 #' @importFrom limma makeContrasts
 #' @importFrom DESeq2 DESeqDataSetFromMatrix DESeq estimateSizeFactors estimateDispersionsGeneEst nbinomWaldTest results
 #' @examples
+#' \donttest{
 #' if (requireNamespace("phyloseq", quietly = TRUE)) {
 #'   data("physeq_16SOTU", package = "DspikeIn")
 #'
@@ -59,7 +60,8 @@
 #'   ps_sal <- phyloseq::subset_samples(
 #'     physeq_16SOTU,
 #'     Animal.type == "Salamander" &
-#'       Diet %in% c("Insectivore", "Carnivore") )
+#'       Diet %in% c("Insectivore", "Carnivore")
+#'   )
 #'
 #'   # Remove taxa with zero counts 
 #'   ps_sal <- phyloseq::prune_taxa(
@@ -67,36 +69,35 @@
 #'     ps_sal
 #'   )
 #'
-#'   # differential abundance test at Genus
+#'   # Differential abundance test at Genus level
 #'   da_res <- perform_and_visualize_DA(
 #'     obj = ps_sal,
 #'     method = "DESeq2",
 #'     group_var = "Diet",
 #'     significance_level = 0.05,
 #'     contrast = c("Insectivore", "Carnivore"),
-#'     target_glom = "Genus")
+#'     target_glom = "Genus"
+#'   )
 #'
 #'   # Visualize results
 #'   if (!is.null(da_res$plot)) print(da_res$plot)
 #'   head(da_res$results)
 #'
-#'   \donttest{
-#'     # Example: multiple contrasts 
-#'     contrast_list <- list(c("Insectivore", "Carnivore"))
-#'     da_multi <- perform_and_visualize_DA(
-#'       obj = ps_sal,
-#'       method = "DESeq2",
-#'       group_var = "Diet",
-#'       significance_level = 0.01,
-#'       contrast = contrast_list,
-#'       target_glom = "Genus",
-#'       global_fdr = TRUE
-#'     )
+#'   # Example: multiple contrasts (optional demonstration)
+#'   contrast_list <- list(c("Insectivore", "Carnivore"))
+#'   da_multi <- perform_and_visualize_DA(
+#'     obj = ps_sal,
+#'     method = "DESeq2",
+#'     group_var = "Diet",
+#'     significance_level = 0.01,
+#'     contrast = contrast_list,
+#'     target_glom = "Genus",
+#'     global_fdr = TRUE
+#'   )
 #'
-#'     # Display a bar plot for significant genera (if found)
-#'     if (!is.null(da_multi[[1]]$bar_plot))
-#'       print(da_multi[[1]]$bar_plot)
-#'   }
+#'   if (!is.null(da_multi[[1]]$bar_plot))
+#'     print(da_multi[[1]]$bar_plot)
+#' }
 #' }
 #'
 #' @export
